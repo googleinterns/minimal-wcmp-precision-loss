2c6c5d6ba61cea9cd1cae8c4acec48759b0cc2aa
Clark5 <wtwang@rice.edu>
2020-07-01 09:45:41 -0500

    add main

---
+++ wcmp/main.cpp
@@ -0,0 +1,27 @@
+//
+// Created by wwt on 6/14/20.
+//
+
+#include <iostream>
+#include <glog/logging.h>
+#include "topology/DCN.h"
+#include "topology/dcnonly/DCNOnlyTopology.h"
+//#include "topology/full/FullTopology.h"
+
+int main() {
+	// initial glog
+	google::InitGoogleLogging("scip");
+
+	// initialize the DCN network
+	dcnonly::DCNOnlyTopology network;
+
+	// find the best routing policy
+	SCIP_RETCODE retcode = network.find_best_dcn_routing();
+	if (retcode != SCIP_OKAY) {
+		LOG(ERROR) << "The SCIP program is wrong.";
+	}
+
+	// analysis the result
+	network.result_analysis();
+	return 0;
+}
